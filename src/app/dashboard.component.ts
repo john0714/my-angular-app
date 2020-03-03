34eec6c6699aae3f72ccae2dfda44c448b33032d
john0714 <john0714@naver.com>
2020-03-03 16:59:23 +0900

    Tutorial - 5 まとめ

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -12,7 +12,9 @@ import { HeroService } from './hero.service';
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = []; // heroes 프로퍼티를 정의
 
-  constructor(private heroService: HeroService) { } // 생성자에서 HeroService를 주입받고 private heroService필드에 할당
+  constructor(
+    private heroService: HeroService
+  ) { } // 생성자에서 HeroService를 주입받고 private heroService필드에 할당
 
   ngOnInit(): void { // ngOnInit() 라이프 사이클 후크에서 Service를 호출하여 Heroes을 얻습니다.
     this.heroService.getHeroes()
